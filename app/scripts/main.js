b23e8aaf8c470d9e896789703e623f1243dd6a14
Francisco Miranda <francisco.miranda@hmhco.com>
2015-05-31 11:43:06 +0100

    Basic layout and dependencies. Remove noise

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,19 @@
-console.log('\'Allo \'Allo!');
+/* global jsyaml, d3 */
+
+'use strict';
+
+var d3Area = d3.select('#stackfile-content');
+var d3AreaWrapper = d3.select('.stack-input');
+
+d3Area.node().addEventListener('input',function() {
+  var stackfileJSON;
+
+  try{
+    stackfileJSON = jsyaml.load(d3Area.node().value);
+    d3AreaWrapper.classed('has-error',false);
+  } catch(e){
+    d3AreaWrapper.classed('has-error',true);
+    return;
+  }
+
+});
